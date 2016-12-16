59f0adf6d57628eb85c08a0d46d46dd124de44ae
张志伟 <wangqingbaidu@gmail.com>
2016-12-16 21:17:29 +0800

    config

---
+++ utils/network_weights2base64.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*- 
+'''
+Authorized  by Vlon Jang
+Created on 2016年12月16日
+Blog: www.wangqingbaidu.cn
+Email: wangqingbaidu@gmail.com
+From Institute of Computing Technology
+©2015-2016 All Rights Reserved.
+'''
+
+import ConfigParser, base64
+def parser_network(net='kwai_fconv_s2_sig.cfg'):
+    
